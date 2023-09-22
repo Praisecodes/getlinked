885f724a859b8fdd9dfcbd56adf21380f0e4224a
Praisecodes <cmancoder@gmail.com>
2023-09-22 22:22:33 +0100

    [update] build mobile nav

--- src/components/molecules/header.tsx
+++ src/components/molecules/header.tsx
@@ -4,10 +4,13 @@ import Image from "next/image";
 import Link from "next/link";
 import NavOptions from '@/json/navOptions.json';
 import { Button } from "../atoms";
+import { useState } from "react";
 
 const Header = (): React.ReactNode => {
+  const [mobileNavOpen, setMobileNavOpen] = useState<boolean>(false);
+
   return (
-    <header className={`border-b font-montserrat text-white border-b-[#FFFFFF2E] bg-transparent flex items-center justify-between py-7 lg:px-16 px-4`}>
+    <header className={`border-b relative font-montserrat text-white border-b-[#FFFFFF2E] bg-transparent flex items-center justify-between py-7 lg:px-16 px-4`}>
       <Link href={`/`}>
         <Image
           src={`/images/getlinked.svg`}
@@ -20,7 +23,7 @@ const Header = (): React.ReactNode => {
 
       <section className={`space-x-24 lg:flex hidden items-center`}>
         <ul className={`space-x-12 text-sm`}>
-          {NavOptions.map((option, index)=>(
+          {NavOptions.map((option, index) => (
             <li key={index} className={`float-left`}>
               <Link href={option.route}>
                 <p>
@@ -32,24 +35,54 @@ const Header = (): React.ReactNode => {
         </ul>
 
         <div className={`w-[9rem] lg:block hidden duration-150`}>
-          <Button onClick={()=>{}}>
+          <Button onClick={() => { }}>
             Register
           </Button>
         </div>
       </section>
 
+      <button onClick={() => { setMobileNavOpen(true) }} className={`p-2 lg:hidden block`}>
+        <Image
+          src={`/icons/hamburger.svg`}
+          alt="Hamburger Icon"
+          width={19}
+          height={19}
+          className={`w-[19px]`}
+        />
+      </button>
+
       {/**Mobile Nav */}
-      <div className={`lg:hidden block`}>
-          <button className={`p-2`}>
-            <Image 
-              src={`/icons/hamburger.svg`}
-              alt="Hamburger Icon"
-              width={19}
-              height={19}
-              className={`w-[19px]`}
+      <div className={`${mobileNavOpen ? "translate-y-0" : "-translate-y-[100%]"} z-50 duration-300 w-full absolute top-0 left-0 py-4 px-14 rounded-md bg-[#150E28]`}>
+        <div className={`flex items-center justify-end`}>
+          <button onClick={() => { setMobileNavOpen(false) }}>
+            <Image
+              src={`/icons/close_nav.svg`}
+              alt="Close Nav"
+              width={23}
+              height={23}
+              className={`w-[30px] object-center`}
             />
           </button>
-        </div>
+        </div> <br />
+
+        <ul className={``}>
+          {NavOptions.map((option, index) => (
+            <li key={index} className={`my-5`}>
+              <Link href={option.route}>
+                <p>
+                  {option.name}
+                </p>
+              </Link>
+            </li>
+          ))}
+        </ul> <br />
+
+        <div className={`w-[10rem]`}>
+          <Button onClick={() => { }}>
+            Register
+          </Button>
+        </div> <br /><br />
+      </div>
     </header>
   )
 }
